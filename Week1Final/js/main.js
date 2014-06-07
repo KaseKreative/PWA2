72dcb923330beec6461b63895c3e88cfc24854eb
KaseKreative <KaseKreative@fullsail.edu>
2014-06-07 13:17:24 -0400

    Week1Activity

--- Week1Final/js/main.js
+++ Week1Final/js/main.js
@@ -1,9 +1,51 @@
 /**
  * Created by KaseTebo on 6/6/14.
  */
+var counter = 0;
+var plus = document.getElementById("add");
+var listSelection = document.getElementById("console");
+var append = document.getElementById("append");
+var save = document.querySelector("#save");
+var fullName = document.querySelector("#name");
+var usersName = document.querySelector("#UserTitle");
+var Name = fullName.value;
 
-    $('#login a').click(function() {
-//        alert("Sign In Please");
+(function(){
+
+$(document).ready(function() {
+
+console.log(Name);
+
+
+
+
+    $(plus).click(function(){
+        counter += 1;
+        if( counter < 4){
+        $(listSelection).clone()
+            .appendTo(append);
+        }
+    });
+});
+
+
+
+    //    Form Requirements
+        $(save).click(function(){
+        if ($(Name).value().length > 0) {
+            alert(boo);
+            $("#save").prop("disabled", false);
+            $('#dashTitle').css("display", "block")
+                .animate({opacity: "1"});
+           $("#name").innerHTML = ('<span>'+Name+'</span>');
+
+        } else {
+            $("#save").prop("disabled", true);
+        }
+    });
+
+
+    $("#login a").click(function() {
         $('#loginModal')
             .css("display", "block")
             .animate({opacity:"1"});
@@ -11,6 +53,8 @@
             .css("display", "block")
             .animate({opacity:"1"});
     });
+
+
 
     $('#loginModalButton').click(function(){
        $('#loginModal')
@@ -20,6 +64,8 @@
     });
 
 
+
+
     $("#LoginX").click(function(){
         $('#loginModal')
             .css("display", "none");
@@ -27,7 +73,5 @@
             .css("display", "none");
     });
 
-//$('+').click(function(){
-//    $("#consoleDiv").clone().append("#consoleDiv");
-//
-//});
+})();
+
