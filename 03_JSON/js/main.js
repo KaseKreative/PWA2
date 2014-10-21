3d53e3742978071338980f33a20f8072d80d99bc
KaseKreative <KaseKreative@fullsail.edu>
2014-10-21 09:26:06 -0400

    Week4MAybe

---
+++ 03_JSON/js/main.js
@@ -0,0 +1,9 @@
+//Your name
+
+var source = $("#some-template").html();
+var template = handlebars.compile(source)
+
+
+
+
+//Declare your document ready.
---
+++ JSON3Practice/js/main.js
@@ -0,0 +1,21 @@
+//Your name
+
+var data = {"students": [
+    {
+        "firstName" : "Kase",
+        "lastName" : "Tebo",
+        "startDate" : {
+            "month" : "September",
+            "day" : "30",
+            "year" : "2013"
+        }
+
+
+    }
+
+]}
+
+
+
+
+//Declare your document ready.
